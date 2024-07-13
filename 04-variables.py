e745437a186e1d69a4509c448e5f6b44766bbc3b
Andrés Urbano <urbanoivan10@gmail.com>
2024-07-13 12:29:51 -0500

    Practicando cpn variables en Python

---
+++ 04-variables.py
@@ -0,0 +1,31 @@
+# nombre ="Juan"
+# print(nombre)
+# edad = 30
+# print(edad)
+
+# edad1 = 20
+# edad2 = 15
+# suma = edad1 + edad2
+# print(suma)
+
+# nombre = input("Dime tu nombre: ")
+# print("tu nombre es: " + nombre)
+
+# primerNombre = "Andrés"
+# SegundoNombre = "Luis"
+# nombres = primerNombre + " " + SegundoNombre
+# print(nombres)
+
+
+nombre = "Tony Soprano"
+Edad = 51
+
+nombre = "Julia"
+apellido = "Roberts"
+
+nombreCompleto = nombre + " " + apellido
+print(nombreCompleto)
+
+curso = "Python"
+print("Estas tomando un curso de: " + " " + curso)
+
---
+++ pasos-git.py
@@ -0,0 +1,6 @@
+# git init
+# git remote
+# git status
+# git add prueba.py o giy add .add
+# git commit -m "Descripcion"
+# git push
