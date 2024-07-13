a637a5f20ca2abd5128da023db8b9ef5692a3b55
Andrés Urbano <urbanoivan10@gmail.com>
2024-07-13 13:19:33 -0500

    Practica de conversones y formateo de cadenas

---
+++ 06-conversiones.py
@@ -0,0 +1,17 @@
+# num1 = 5.8
+# print(type(num1))
+# num2 = int(num1)
+# print(type(num2))
+
+edad = int(input("Dime tu edad: "))
+print(type(edad))
+
+
+
+
+
+
+
+
+
+
---
+++ 07-formateo-cadenas.py
@@ -0,0 +1,7 @@
+# x = 10
+# y = 5
+
+# print("Mis numeros son {} y {} ".format(x,y))
+color = "rojo"
+matricula = 12345
+print(f"El auto es {color} y su matricula es {matricula}")
