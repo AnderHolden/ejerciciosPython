31b76b7f84f773a3a9d010647b42bbb9ba887090
Andrés Urbano <urbanoivan10@gmail.com>
2024-07-27 13:32:47 -0500

    practicando con python 3

---
+++ 13-propiedades.py
@@ -0,0 +1,18 @@
+n1 = "caro"
+n2 = "lina"
+print(n1+n2)
+
+print(n1*5)
+
+poema = """
+Esta es una linea del poema
+esta seria otra linea
+y esta seria la tercera linea
+"""
+print(poema)
+
+print(len(poema))
+
+palabra = "hola"
+print(len(palabra))
+print("poema" in poema)
