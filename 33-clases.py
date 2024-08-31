01ed4eeedf0da4bddc2eeb1a041c75385680a48a
Ander Holden <urbanoivan10@gmail.com>
2024-08-31 09:00:21 -0500

    Ejercicio con clases y objetos

---
+++ 33-clases.py
@@ -0,0 +1,35 @@
+class Parajo:
+   pass
+piolin = Parajo()
+mi_pajaro = Parajo()
+
+print(piolin)
+
+
+class Personaje:
+   pass
+
+harry_Potter = Personaje
+
+print(harry_Potter)
+
+class Dinosaurio:
+   pass
+
+velociraptor = Dinosaurio
+tiranosaurio_rex = Dinosaurio
+branquiosaurio = Dinosaurio
+
+print(velociraptor)
+print(tiranosaurio_rex)
+print(branquiosaurio)
+
+class PlataformasStreaming:
+   pass
+netflix = PlataformasStreaming
+hbo_max = PlataformasStreaming
+amazon_prime_video = PlataformasStreaming
+
+print(netflix)
+print(hbo_max)
+print(amazon_prime_video)
